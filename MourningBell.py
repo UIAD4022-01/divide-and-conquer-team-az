aa1fe9dda61040e42b462033fa11027a3ecae7d4
Ana <ana.honarmandian@gmail.com>
2024-04-14 15:51:38 +0330

    Defined mid variable and for loop to find edjes

--- MourningBell.py
+++ MourningBell.py
@@ -2,3 +2,11 @@ def length_three_cycle(adj_matrix):
     def divide_and_conquer(start, end):
         if end - start <= 2:
             return 'NO'
+        
+        mid = (start + end) // 2
+
+   
+        for i in range(start, mid - 1):
+            for j in range(mid, end - 1):
+                if adj_matrix[i][j] == 1 and adj_matrix[j][i] == 1:
+                    return 'YES'
