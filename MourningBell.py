d202cc624768672b759e09fa634cd55f92ecc602
Ana <ana.honarmandian@gmail.com>
2024-04-14 15:59:07 +0330

    saving result by recalling length_three_cycle func

--- MourningBell.py
+++ MourningBell.py
@@ -1,6 +1,6 @@
 def length_three_cycle(adj_matrix):
-    n=len(adj_matrix)
-    
+    n = len(adj_matrix)
+
     def divide_and_conquer(start, end):
         if end - start <= 2:
             return 'NO'
@@ -19,4 +19,10 @@ def length_three_cycle(adj_matrix):
         return 'YES' if left_result == 'YES' or right_result == 'YES' else 'NO'
     
     return divide_and_conquer(0, n)
+
+
+n = int(input())
+adjacency_matrix = [list(map(int, input().split())) for _ in range(n)]
+result = length_three_cycle(adjacency_matrix)
+print(result)
 
